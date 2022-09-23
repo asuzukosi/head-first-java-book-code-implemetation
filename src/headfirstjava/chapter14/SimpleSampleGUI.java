ccf61de81cbbd86da0b0658f40fe362ad86c3a9b
kosi <kosisochukwu.asuzu@e4email.net>
2022-09-23 21:16:09 +0100

    feat: implement jlist and jcheckbox in the application

--- src/headfirstjava/chapter14/SimpleSampleGUI.java
+++ src/headfirstjava/chapter14/SimpleSampleGUI.java
@@ -1,10 +1,19 @@
 package headfirstjava.chapter14;
 import java.awt.*;
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
 import java.awt.event.*;
 
-public class SimpleSampleGUI implements ActionListener {
+public class SimpleSampleGUI implements ActionListener, ItemListener, ListSelectionListener {
     JTextArea text;
+    Boolean canSet = true;
+    JCheckBox checkBox = new JCheckBox();
+    String [] listEntries = {"alpha", "beta", "gamma", "delta", "epsilon", "zeta", "eta", "theta "};
+    JList<String> list = new JList<String>(listEntries);
+
+
     public static void main (String[] args) {
         SimpleSampleGUI gui = new SimpleSampleGUI();
         gui.go();
@@ -21,6 +30,7 @@ public class SimpleSampleGUI implements ActionListener {
         JButton button = new JButton("Just Click It");
         button.addActionListener(this);
 
+
         // initiate text area
         text = new JTextArea(10,20);
         text.setLineWrap(true);
@@ -35,7 +45,21 @@ public class SimpleSampleGUI implements ActionListener {
 
         // add panel and button to the frame
         frame.getContentPane().add(BorderLayout.CENTER, panel);
-        frame.getContentPane().add(BorderLayout.SOUTH, button);
+        JPanel otherPanel = new JPanel();
+        
+        frame.getContentPane().add(BorderLayout.SOUTH, otherPanel);
+
+        
+        JScrollPane scroller2 = new JScrollPane(list);
+        scroller2.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        scroller2.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        list.setVisibleRowCount(4);
+        list.addListSelectionListener(this);
+
+        otherPanel.add(button);
+        otherPanel.add(checkBox);
+        otherPanel.add(scroller2);
+
 
         frame.setSize(300, 300);
         frame.setVisible(true);
@@ -44,6 +68,26 @@ public class SimpleSampleGUI implements ActionListener {
 
 
     public void actionPerformed(ActionEvent ev) {
-        text.append("button clicked \n");
+        if(canSet){
+            text.append("button clicked \n");
         }
+        }
+
+    @Override
+    public void itemStateChanged(ItemEvent e) {
+        System.out.println("I am clicked");
+        if(checkBox.isSelected()){
+            this.canSet = true;
+        } else {
+            this.canSet = false;
+        }
+    }
+
+    @Override
+    public void valueChanged(ListSelectionEvent e) {
+        if( !e.getValueIsAdjusting()) {
+            String selection = (String) list.getSelectedValue();
+            System.out.println(selection);
+        }  
+    }
 }
