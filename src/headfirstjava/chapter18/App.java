11ce3b5bb6dbf0242bdce83556523607d6c964c2
kosi <kosisochukwu.asuzu@e4email.net>
2022-10-08 10:49:58 +0100

    feat: start working on chapter 18

---
+++ src/headfirstjava/chapter18/App.java
@@ -0,0 +1,7 @@
+package headfirstjava.chapter18;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("We look at the various deployment techniques for java applications");
+    }
+}
